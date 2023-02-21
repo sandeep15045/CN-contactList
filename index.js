96f3bb8abc931a0829bfe52ba339cb02c206d574
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-21 22:24:45 +0530

    created http server

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const agrs =process.argv;
-console.log(agrs);
-var args = process.argv;
-args.forEach((val, index) => {
-    console.log(`${index}: ${val}`);
- });
+var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.write('Hello World!');
+//   res.end();
+// }).listen(8000);
+
+var server = http.createServer(function (req, res) {   // 2 - creating server
+    //handle incomming requests here..
+});
+server.listen(8000); //3 - listen for any incoming requests
+console.log('Node.js web server at port 5000 is running..')
