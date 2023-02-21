90eedcec966577668b70f1d4457532a7ae6402c3
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-21 22:21:04 +0530

    forEach

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const agrs =process.argv;
 console.log(agrs);
-
-const b = process.argv;
-console.log(b);
-const [a] = process.argv;
-console.log(a[0],a[1],a[2]); // node
-
+var args = process.argv;
+args.forEach((val, index) => {
+    console.log(`${index}: ${val}`);
+ });
