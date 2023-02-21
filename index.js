47a39e7c0bc2088f034c34c488cf9d9744387cf5
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-22 01:23:44 +0530

    read html file and rendered

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const http= require('http')
 const port=8000;
+const fs=require('fs');
 
 function requestHandler(req,res){
-    console.log(req.url)
-    res.writeHead(200,{'content-type':'text/html'});
-    res.end('gotcha')
+    console.log(req.url);
+    fs.readFile('./index.html',function(err,data){
+        if(err)
+        {
+            console.log('error',err);
+            return res.end('<h1>erro</h1>');
+        }
+        return res.end(data);
+    })
+
 }
 const server=http.createServer(requestHandler);
 server.listen(port,function(err){
