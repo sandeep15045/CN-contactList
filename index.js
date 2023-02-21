b1044ab20357940efbed8c7b6ed88d629471c0aa
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-21 22:32:18 +0530

    added package.json and http server error

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 var http = require('http');
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.write('Hello World!');
-//   res.end();
-// }).listen(8000);
 
-var server = http.createServer(function (req, res) {   // 2 - creating server
-    //handle incomming requests here..
-});
-server.listen(8000); //3 - listen for any incoming requests
-console.log('Node.js web server at port 5000 is running..')
+http.createServer(function (req) {
+    if(req){
+        console.log(req);
+        return;
+    }
+    console.log("server running on port: ",port)    
+}).listen(8000);
