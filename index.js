b7b33248eca7bf183c3ee3c31d34f40fbd697150
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-22 22:52:01 +0530

    assets loaded and css js too

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app=express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded());
-
+app.use(express.static('assets'));
 var contactList=[
     {
         name:"sandeep",
