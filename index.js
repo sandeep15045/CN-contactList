b1f03aa18b899725decbd5e3fedee6aa6d6b96c6
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-22 18:07:34 +0530

    29.11 sending response express server

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express= require('express');
 const port=8000;
 
 const app=express();
+
+app.get('/',function(req,res){
+    res.send('cool it is running');
+})
+
 app.listen(port,function(err){
     if(err)
     {
