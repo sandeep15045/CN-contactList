607907b2dad5c7500c4f1ce55b83e358d20c683e
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-22 22:10:18 +0530

    rendered contact list in view

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const path=require('path');
 const app=express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+var contactList=[
+    {
+        name:"sandeep",
+        number:"11111"
+    },
+    {
+        name:"Lucky",
+        number:"22222"
+    },
+    {
+        name:"Tarun",
+        number:"33333"
+    }
+]
 app.get('/',function(req,res){
-    console.log(__dirname);
-    return res.render('home',{'title':"My contacts list"});
+    return res.render('home',{
+    'title':"My contacts list", 
+    contact_list:contactList
+    });
     
 })
 
