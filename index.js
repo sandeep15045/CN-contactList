943c77c2f1500e983304e057e5c8f24172123277
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-22 18:04:26 +0530

    express server

--- index.js
+++ index.js
@@ -1,39 +1,13 @@
-const http= require('http')
+const express= require('express');
 const port=8000;
-const fs=require('fs');
 
-function requestHandler(req,res){
-    console.log(req.url);
-
-    res.writeHead(200,{'content-type':'text/html'});
-    let filepath;
-    switch(req.url){
-        case '/':
-            filepath='./index.html'
-            break;
-        case '/profile':
-            filepath='./profile.html'
-            break;
-        default:
-            filepath='./404.html'
+const app=express();
+app.listen(port,function(err){
+    if(err)
+    {
+        console.log('error ',err)
     }
-    
-    fs.readFile(filepath,function(err,data){
-        if(err)
-        {
-            console.log('error',err);
-            return res.end('<h1>erro</h1>');
-        }
-        return res.end(data);
-    })
+   
+    console.log('server runnning on port: ',port)
 
-}
-const server=http.createServer(requestHandler);
-server.listen(port,function(err){
-    if(err){
-        console.log(err);
-        return;
-    }
-    //importtant-> differnet way of writing port:
-    console.log("server running ${{port}} on port : ",`port is ${port} `,port);
 });
