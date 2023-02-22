ae50470cb8adc3014ec3fe7052f46afa92a7111a
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-23 01:00:57 +0530

    rendered contact from db

--- index.js
+++ index.js
@@ -8,25 +8,20 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded());
 app.use(express.static('assets'));
-var contactList=[
-    {
-        name:"sandeep",
-        number:"11111"
-    },
-    {
-        name:"Lucky",
-        number:"22222"
-    },
-    {
-        name:"Tarun",
-        number:"33333"
-    }
-]
+
 app.get('/',function(req,res){
-    return res.render('home',{
-    'title':"My contacts list", 
-    contact_list:contactList
+
+    Contact.find({},function(err,contactList){
+        if(err){
+            console.log('error in fetchin db', err);
+            return;
+        }
+        return res.render('home',{
+            'title':"My contacts list", 
+            contact_list:contactList
+            });
     });
+
     
 })
 app.post('/create',function(req,res){
